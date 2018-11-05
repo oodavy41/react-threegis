2cf79f8f22c9e2948f9bda7247a5ee13e4e8823d
CoColate <c@cocolate.cc>
2018-11-05 15:48:23 +0800

    border drawer

--- src/threeScene.js
+++ src/threeScene.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import * as OBJLoader from "three-obj-loader";
 import { threeObj, animateObj, aniType } from "./threeObj";
+import { Map } from "react-arcgis";
 OBJLoader(THREE);
 
 export var TScene = {
@@ -135,12 +136,28 @@ export var TScene = {
 
     let line8 = this.makeLine(
       [
-        [0, 0, 0],
-        [4500, 4500, 100],
-        [4000, 2000, 200],
-        [100, 4900, 300]
+        {
+          start: [0, 0, 50],
+          end: [4500, 4500, 100],
+          size: 200
+        },
+        {
+          end: [4000, 2000, 200],
+          start: [4500, 4500, 100],
+          size: 200
+        },
+        {
+          end: [100, 4900, 300],
+          start: [4000, 2000, 200],
+          size: 200
+        },
+        {
+          start: [100, 4900, 300],
+          end: [0, 0, 0],
+          size: 200
+        }
       ],
-      0xffffff,
+      0xff55ff,
       true
     );
     this.scene.add(line8);
@@ -284,24 +301,91 @@ export var TScene = {
     this.scene.add(cubez);
   },
 
-  makeLine(array, color, loop) {
+  /**
+   *
+   * @param {*} array [{start:[x,y,z],end:[x,y,z],size:widthNumber},...]
+   * @param {*} color
+   */
+  makeLine(array, color) {
     let lineGeomentry = new THREE.Geometry();
     array.forEach(element => {
+      let start = element.start;
+      let end = element.end;
+      let width = element.size / 2;
+      let tan = (end[1] - start[1]) / (end[0] - start[0]);
+      let angle = Math.atan2(
+        end[1] - start[1],
+        end[0] - start[0]
+      );
+      let pos = lineGeomentry.vertices.length;
+      let dx = width * Math.sin(angle);
+      let dy = width * Math.cos(angle);
+      console.log(width, angle);
+      console.log(dx * dx + dy * dy);
       lineGeomentry.vertices.push(
-        new THREE.Vector3().fromArray(this.tranToArc(element))
+        new THREE.Vector3().fromArray(
+          this.tranToArc([
+            start[0] + dx,
+            start[1] - dy,
+            start[2]
+          ])
+        )
       );
+      lineGeomentry.vertices.push(
+        new THREE.Vector3().fromArray(
+          this.tranToArc([
+            start[0] - dx,
+            start[1] + dy,
+            start[2]
+          ])
+        )
+      );
+      lineGeomentry.vertices.push(
+        new THREE.Vector3().fromArray(
+          this.tranToArc([end[0] + dx, end[1] - dy, end[2]])
+        )
+      );
+      lineGeomentry.vertices.push(
+        new THREE.Vector3().fromArray(
+          this.tranToArc([end[0] - dx, end[1] + dy, end[2]])
+        )
+      );
+      lineGeomentry.vertices.push(
+        new THREE.Vector3().fromArray(this.tranToArc(start))
+      );
+      for (let i = 0; i < 16; i++) {
+        lineGeomentry.vertices.push(
+          new THREE.Vector3().fromArray(
+            this.tranToArc([
+              start[0] + width * Math.sin((Math.PI * i) / 8),
+              start[1] + width * Math.cos((Math.PI * i) / 8),
+              start[2]
+            ])
+          )
+        );
+      }
+      lineGeomentry.faces.push(
+        new THREE.Face3(pos, pos + 2, pos + 1)
+      );
+      lineGeomentry.faces.push(
+        new THREE.Face3(pos + 1, pos + 2, pos + 3)
+      );
+
+      for (let i = 0; i < 16; i++) {
+        lineGeomentry.faces.push(
+          new THREE.Face3(
+            pos + 4,
+            i < 15 ? pos + 6 + i : pos + 5,
+            pos + 5 + i
+          )
+        );
+      }
     });
-    if (loop) {
-      return new THREE.LineLoop(
-        lineGeomentry,
-        new THREE.LineBasicMaterial({ color: color })
-      );
-    } else {
-      return new THREE.Line(
-        lineGeomentry,
-        new THREE.LineBasicMaterial({ color: color })
-      );
-    }
+
+    return new THREE.Mesh(
+      lineGeomentry,
+      new THREE.LineBasicMaterial({ color: color })
+    );
   },
 
   makeDot(location, color, radius) {
